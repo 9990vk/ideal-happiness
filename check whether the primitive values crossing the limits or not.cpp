b32ce500774004bd014cf1c70390dd4324f97e88
VISHAL KULAKRNI <79957914+9990vk@users.noreply.github.com>
2021-03-09 09:26:50 +0530

    pract programs

---
+++ check whether the primitive values crossing the limits or not.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+ int  main()
+ {
+char gender  ='F' ;
+bool married =1 ;
+unsigned short numberofsons =  2 ;
+short yearofherappointment = 2009 ;
+unsigned int salary = 1500000 ;
+double height = 79.48 ;
+float gpa =  4.69 ;
+long  salarydrawn = 12047235 ;
+long long  balance = 995324987 ;
+
+cout << "The Gender is : " 				<< gender 			<< "\n"  ; 
+cout << "TIs she married?  : " 			<< married 			<< "\n";
+cout << "Number of sons she has : : " 	<< numberofsons 		<< "\n" ;
+cout << "Year of her appointment :  : "	<< yearofherappointment 	<< "\n" ;
+cout << "Salary for a year : "			<< salary 	<< "\n" ;
+cout << "Height is : " 					<< height	<< "\n";
+cout << "GPA is : "						<< gpa 		<< "\n";
+cout << "Salary drawn upto : "			<< salarydrawn			<< "\n";
+cout << "Balance till : "				<< balance		 	<< "\n";
+
+	return 0;
+}
---
+++ size of data types.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+ int  main()
+ {
+
+cout << "Size of fundamental datatypes \n ";
+cout << "The size of(int) =  " <<sizeof(int)<<  "bytes \n" ;
+cout << "The size of(float) =" <<sizeof(float) << "bytes\n ";
+cout << "The size of(bool) =" <<sizeof(bool) << "bytes\n ";
+cout << "The size of(char) =" <<sizeof(char) << "bytes \n ";
+cout << "The size of(double) =" <<sizeof(double) << "bytes\n ";
+cout << "The sizeof(short) is =" <<sizeof(int)<<  "bytes \n" ;
+cout << "The size of(long) =  " <<sizeof(long)<<  "bytes \n" ;
+cout << "The size of(longlong) =  " <<sizeof(long long)<<  "bytes \n" ;
+cout << "The size of(long double) =  " <<sizeof(long double)<<  "bytes \n" ;
+
+	return 0;
+}
+
+
+ 
